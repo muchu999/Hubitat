33a10991eca8a380e980535ce953678abdd5a302
muchu999 <33784829+muchu999@users.noreply.github.com>
2020-02-14 13:13:51 -0500

    Update Fibaro FBGS-222 Child Digital Input.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Digital Input.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Digital Input.groovy
@@ -1,7 +1,7 @@
 metadata {
 	definition (name: "Fibaro FGBS-222 Child Digital Input", namespace: "christi999", author: "") {
 		// capability "Refresh"  //didn't find a way to do it manually, will get notifications otherwise...  
-		capability "Contact Sensor"
+		capability "ContactSensor"
 	}
 }
 
