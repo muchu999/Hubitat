93c919c34b2aa061d989ad83df926c50fd6298b4
muchu999 <33784829+muchu999@users.noreply.github.com>
2020-06-08 13:38:19 -0400

    Update Fibaro FBGS-222 Child Temperature Sensor.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Temperature Sensor.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Temperature Sensor.groovy
@@ -2,7 +2,7 @@ metadata {
 	definition (name: "Fibaro FGBS-222 Child Temperature Sensor", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Test-Packages/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Temperature%20Sensor.groovy") {
 		capability "Refresh"
 		capability "Sensor"
-        capability "Temperature Measurement"
+		capability "Temperature Measurement"
 	}
 }
 
