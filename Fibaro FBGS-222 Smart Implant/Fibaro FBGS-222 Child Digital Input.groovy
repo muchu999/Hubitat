495da1ecdf604cccf8d5d859f5876da2b6b9433f
muchu999 <33784829+muchu999@users.noreply.github.com>
2021-02-02 20:23:47 -0500

    Update Fibaro FBGS-222 Child Digital Input.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Digital Input.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Digital Input.groovy
@@ -1,5 +1,5 @@
 metadata {
-	definition (name: "Fibaro FGBS-222 Child Digital Input", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Test-Packages/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Digital%20Input.groovy") {
+	definition (name: "Fibaro FGBS-222 Child Digital Input", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/master/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Digital%20Input.groovy") {
 		// capability "Refresh"  //didn't find a way to do it manually, will get notifications otherwise...  
 		capability "ContactSensor"
 	}
