0a58e68dd67ab2b931c3237edad6128cd2b40956
muchu999 <33784829+muchu999@users.noreply.github.com>
2021-02-02 20:14:35 -0500

    Create Fibaro FBGS-222 Child Humidity Sensor.groovy

---
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Humidity Sensor.groovy
@@ -0,0 +1,11 @@
+metadata {
+	definition (name: "Fibaro FGBS-222 Child Humidity Sensor", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Test-Packages/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Relative Humidity%20Sensor.groovy") {
+		capability "Refresh"
+		capability "Sensor"
+		capability "Relative Humidity Measurement"
+	}
+}
+
+void refresh() {
+	parent.childRefresh(device.deviceNetworkId)
+}
