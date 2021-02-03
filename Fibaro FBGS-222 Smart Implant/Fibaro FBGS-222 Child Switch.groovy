9afa60c00dcd8d19bcf87596b2c4fdeeca60cdfd
muchu999 <33784829+muchu999@users.noreply.github.com>
2021-02-02 20:22:39 -0500

    Update Fibaro FBGS-222 Child Switch.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Switch.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Switch.groovy
@@ -1,5 +1,5 @@
 metadata {
-	definition (name: "Fibaro FGBS-222 Child Switch", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Test-Packages/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Switch.groovy") {
+	definition (name: "Fibaro FGBS-222 Child Switch", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/master/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Switch.groovy") {
 		capability "Actuator"
 		capability "Refresh"
 		capability "Sensor"
