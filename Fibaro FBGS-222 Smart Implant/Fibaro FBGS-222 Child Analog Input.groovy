758283c31af3eefaaf5ad2f78577ff8b4781bbfc
muchu999 <33784829+muchu999@users.noreply.github.com>
2021-02-02 20:21:02 -0500

    Update Fibaro FBGS-222 Child Analog Input.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Analog Input.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Child Analog Input.groovy
@@ -1,5 +1,5 @@
 metadata {
-	definition (name: "Fibaro FGBS-222 Child Analog Input", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Test-Packages/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Analog%20Input.groovy") {
+	definition (name: "Fibaro FGBS-222 Child Analog Input", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/Master/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Child%20Analog%20Input.groovy") {
 		capability "Refresh"
 		capability "Voltage Measurement"
 		capability "Sensor"
