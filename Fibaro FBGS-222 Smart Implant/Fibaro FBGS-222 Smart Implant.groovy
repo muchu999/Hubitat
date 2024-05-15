57c7f2a23c00aa3d25d4c65ba7345e1adddd2d47
muchu999 <33784829+muchu999@users.noreply.github.com>
2024-05-15 12:03:02 -0400

    Update Fibaro FBGS-222 Smart Implant.groovy

--- Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Smart Implant.groovy
+++ Fibaro FBGS-222 Smart Implant/Fibaro FBGS-222 Smart Implant.groovy
@@ -10,6 +10,7 @@
 * Licensing:
 *
 * Version Control:
+* 1.7.8 - Adding parameter numbers to descriptions.
 * 1.7.7 - Modified reinstall command confirmation step, added legacy/normal option for alarm NO NC logic, defaults to legacy. Added logical operators and water/pressure capabilities to analog child.
 * 1.7.6 - Modified temperature formatting to avoid use scientific notation in reporting
 * 1.7.5 - Added support for DHT-22 temperature and humidity external sensor
@@ -40,7 +41,7 @@
 * This code is based on the original design from @boblehest on Github
 */
 
-public static String version()      {  return "1.7.7"  }
+public static String version()      {  return "1.7.8"  }
 metadata {
 	definition (name: "Fibaro FGBS-222 Smart Implant", namespace: "christi999", author: "", importUrl: "https://raw.githubusercontent.com/muchu999/Hubitat/master/Fibaro%20FBGS-222%20Smart%20Implant/Fibaro%20FBGS-222%20Smart%20Implant.groovy") {	
 		command( "Reinstall")
